f66eeb977694cd7d234a185530ccdf9e2b59d96f
Graham Shroll <gmshroll@databoxsystems.com>
2021-02-01 16:29:28 +0000

    remove body parser

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 const express = require('express');
 const app = express();
 const path = require("path");
-const bodyParser = require('body-parser');
 
-app.use(bodyParser);
+app.use(express.urlencoded()); //Parse URL-encoded bodies
+
+app.get("/report", (req, res) => {
+	console.log("---- inbound at " + new Date() + " ----");
+	console.log(req.query);
+	console.log(req.body);
+	res.send("OK");
+});
+
+app.get('/hello', function(req, res){
+  res.send('Hello World');
+});
+
 
 app.get("/", (req, res) => {
 	res.sendFile(__dirname + "/static/app.htm");
 });
 
 app.use("/", express.static('static'));
-
-app.all("/report", function(req, res){
-	console.log("---- inbound at " + new Date() + " ----");
-	console.log(req.query);
-	console.log(req.body);
-	res.send("OK");
-});
 
 console.log(":3020");
 app.listen(3020);
